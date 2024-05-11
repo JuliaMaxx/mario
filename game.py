6adbe56aaec9d35a3df867fe21095a9f3af4c4bf
Julia Max <jjuliamaxxx@gmail.com>
2024-05-11 14:53:27 +0200

    chore: add comments

--- game.py
+++ game.py
@@ -1,9 +1,11 @@
 import pygame
 from sys import exit
 
+# initialization
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
 
+# function to toggle fullscreen mode
 def toggle_fullscreen():
     fullscreen = not screen.get_flags() & pygame.FULLSCREEN
     pygame.display.set_mode((800, 600), pygame.FULLSCREEN if fullscreen else 0)
@@ -13,7 +15,8 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-        
+            
+        # toggle fullscreen when F11 is pressed
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_F11:
                 pygame.display.toggle_fullscreen()
