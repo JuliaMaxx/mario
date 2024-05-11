722d9566333707a7baab8f98eaf14b44918224e5
Julia Max <jjuliamaxxx@gmail.com>
2024-05-11 14:42:58 +0200

    refactor: change screen size

--- game.py
+++ game.py
@@ -2,7 +2,13 @@ import pygame
 from sys import exit
 
 pygame.init()
-screen = pygame.display.set_mode((1000, 600))
+screen = pygame.display.set_mode((800, 600))
+
+# Function to toggle fullscreen mode
+def toggle_fullscreen():
+    screen = pygame.display.get_surface()
+    fullscreen = not screen.get_flags() & pygame.FULLSCREEN
+    pygame.display.set_mode((800, 600), pygame.FULLSCREEN if fullscreen else 0)
 
 while True:
     for event in pygame.event.get():
